ea04689a9e2c5cb1abf9ff5b72bf82891bf18a7d
JoaquinAdan <rodriguezjdev@gmail.com>
2024-08-06 14:58:18 -0300

    feat: Add ProductHttpService implementation
    This commit adds the implementation of the `ProductHttpService` class, which implements the `ProductService` interface. The `ProductHttpService` class provides methods for fetching, updating, creating, and finding products using HTTP requests. This implementation allows the application to interact with an external API to perform CRUD operations on products.

---
+++ src/app/services/product-http.service.ts
@@ -0,0 +1,44 @@
+import { UpdateProductDto, CreateProductDto } from '../dtos/product.dto'
+import { ProductService } from '../models/product-service.model'
+import { Product } from '../models/product.model'
+
+export class ProductHttpService implements ProductService {
+  private url = 'https://api.escuelajs.co/api/v1/products'
+
+  async getAll(): Promise<Product[]> {
+    const promise = await fetch(this.url)
+    const rta: Product[] = await promise.json()
+    return rta
+  }
+
+  async update(id: Product['id'], changes: UpdateProductDto): Promise<Product> {
+    const data = await fetch(`${this.url}/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const rta: Product = await data.json()
+    return rta
+  }
+
+  async create(dto: CreateProductDto): Promise<Product> {
+    const response = await fetch(this.url, {
+      method: 'POST',
+      body: JSON.stringify(dto),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const rta: Product = await response.json()
+    return rta
+  }
+
+  async findOne(id: Product['id']): Promise<Product | undefined> {
+    const response = await fetch(`${this.url}/${id}`)
+    if (response.status === 404) return undefined
+    const rta: Product = await response.json()
+    return rta
+  }
+}
